7da6c8ca662ec2d4b339e836d64c0a7b26062f42
httuongvi <huynhthituongvi.ltp22@gmail.com>
2026-07-07 16:41:12 +0700

    Initial Commit

---
+++ MiniFlix/MiniFlixApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniFlixApp.swift
+//  MiniFlix
+//
+//  Created by Tuong Vi on 7/7/26.
+//
+
+import SwiftUI
+
+@main
+struct MiniFlixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
