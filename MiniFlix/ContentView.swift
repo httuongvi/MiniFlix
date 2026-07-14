195a2e0e01083633c836015fa55c0fd463cfb848
httuongvi <huynhthituongvi.ltp22@gmail.com>
2026-07-14 20:00:07 +0700

    Complete day 05 exercises

--- MiniFlix/ContentView.swift
+++ MiniFlix/ContentView.swift
@@ -8,68 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
-    let movieSample: Movie = Movie(
-        title: "Movie 1",
-        overview: "Movie 1 is very exciting Movie 1 is very exciting Movie 1 is very exciting Movie 1 is very exciting Movie 1 is very exciting Movie 1 is very exciting Movie 1 is very exciting",
-        posterPath: nil,
-        voteAverage: 3.5
-        
-    )
-    
+    ///////////////////////////
     @State private var totalVotes: Int = 0
+    
+    
+    
     
     var body: some View {
         let _ = Self._printChanges()
         
-        MovieCardView(movie: movieSample)
-        VStack {
-            Button("Bấm để vote tổng: \(totalVotes)") {
-                totalVotes += 1
-            }
-        }
+        MaintabView()
     }
+    ////////////////
+    ////////////////
+    
     
 }
 
-struct MovieCardView: View{
-    let movie: Movie
+
+
+///////////////////////
+struct MaintabView: View {
+    @State private var selectedTab: Int = 0
     var body: some View {
         let _ = Self._printChanges()
-        VStack{
-            HStack{
-                Image(systemName: "film")
-                    .font(.system(size: 60))
-                
-                
-                VStack{
-                    Text(movie.title)
-                        .font(.largeTitle)
-                        .bold()
-                    
-                    Text(movie.overview)
-                        .font(.body)
-                        .foregroundColor(.gray)
-                        .lineLimit(2)
-                        
+        TabView(selection: $selectedTab) {
+            MovieListView()
+                .tabItem {
+                    Label("Phim", systemImage: "play.house.fill")
                 }
-                .padding(2)
-                
-                HStack (spacing: 2){
-                    Text(String(movie.voteAverage))
-                        .font(.headline)
-                    Image(systemName: "star.fill")
-                        .foregroundColor(.yellow)
-                }
-                    
-            }
-            .background(Color(.systemBackground))
-            .cornerRadius(15)
-            .shadow(color: Color.black.opacity(0.1), radius: 5, x: 0, y: 2)
+                .tag(0)
             
-            Spacer()
+            FavoriteMovieView()
+                .tabItem{
+                    Label("Yêu thích", systemImage: "heart.fill")
+                }
+                .tag(1)
         }
-                 
-                 
+    }
+}
+
+struct FavoriteMovieView: View {
+    var body: some View {
+        Text("Favorite Movie View")
+            .font(.largeTitle)
+            .bold()
     }
 }
 
---
+++ MiniFlix/MovieDetailView.swift
@@ -0,0 +1,42 @@
+//
+//  MovieDetailView.swift
+//  MiniFlix
+//
+//  Created by Tuong Vi on 14/7/26.
+//
+
+import SwiftUI
+
+struct MovieDetailView: View {
+    let movie: Movie
+    var body: some View {
+        VStack{
+            Text(movie.title)
+                .font(.largeTitle)
+                .bold()
+            
+            Image(systemName: "film")
+                .font(.system(size: 200))
+                .frame(width: 250, height: 300)
+                .background(.gray.opacity(0.3))
+            
+            Text(movie.overview)
+                .font(.body)
+            
+            HStack(alignment: .center, spacing: 10){
+                Text("\(String(movie.voteAverage))/10")
+                    .font(.headline)
+                    .bold()
+                Image(systemName: "star.fill")
+                    .font(.system(size: 20))
+                    .foregroundColor(.yellow)
+            }
+        }
+        .padding(20)
+        .frame(maxHeight: .infinity, alignment: .top)
+        .navigationTitle("Detail \(movie.title)")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+
